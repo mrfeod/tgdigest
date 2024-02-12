f4b1ca4f7272e3191fdb5e67525bd248e5bca765
Anton Sosnin <antsosnin@yandex.ru>
2024-02-12 21:33:23 +0200

    Add /video endpoint

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ mod tg;
 mod util;
 mod workers;
 
+use std::process::Command;
+
 use crate::card_renderer::CardRenderer;
 use crate::cli::*;
 use crate::html_renderer::HtmlRenderer;
@@ -157,6 +159,123 @@ async fn digest(
     content::RawHtml(digest)
 }
 
+#[get("/render/<mode>/<channel>?<replies>&<reactions>&<forwards>&<views>&<top_count>&<editor_choice>&<from_date>&<to_date>")]
+async fn video(
+    mode: &str,
+    channel: &str,
+    replies: usize,
+    reactions: usize,
+    forwards: usize,
+    views: usize,
+    top_count: Option<usize>,
+    editor_choice: Option<i32>,
+    from_date: Option<i64>,
+    to_date: Option<i64>,
+) -> Option<NamedFile> {
+    let app = &APP;
+    let task = Task::from_cli(&app.args);
+    let task = Task {
+        command: Commands::Cards {
+            replies,
+            reactions,
+            forwards,
+            views,
+        },
+        mode: mode.to_string(),
+        channel_name: channel.to_string(),
+        top_count: top_count.unwrap_or(task.top_count),
+        editor_choice_post_id: editor_choice.unwrap_or(task.editor_choice_post_id),
+        from_date: from_date.unwrap_or(task.from_date),
+        to_date: to_date.unwrap_or(task.to_date),
+        ..task
+    };
+    println!("Working on task: {}", task.to_string().unwrap());
+
+    let tg_task = task.clone();
+    let handle = spawn_blocking(|| async {
+        let tg = tg::TelegramAPI::create().await.unwrap();
+        let client = tg.client();
+        workers::tg::get_top_posts(client, tg_task).await
+    })
+    .await
+    .unwrap();
+
+    let post_top = match handle.await {
+        Ok(post_top) => post_top,
+        Err(e) => {
+            println!("Error: {}", e);
+            return None;
+        }
+    };
+
+    let render_context = match workers::cards::create_context(post_top, task.clone()) {
+        Ok(render_context) => render_context,
+        Err(e) => {
+            println!("Error: {}", e);
+            return None;
+        }
+    };
+
+    image(channel).await;
+
+    let rendered_html = match app.html_renderer.render(
+        format!("{}/render_template.html", task.mode).as_str(),
+        &render_context,
+    ) {
+        Ok(rendered_html) => rendered_html,
+        Err(e) => {
+            println!("Error: {}", e);
+            return None;
+        }
+    };
+    println!(
+        "Render file rendered to html: lenght={}",
+        rendered_html.len()
+    );
+
+    let card_renderer = CardRenderer::new().await.unwrap();
+    match card_renderer
+        .render_html(&app.ctx.output_dir, &rendered_html)
+        .await
+    {
+        Ok(_) => (),
+        Err(e) => {
+            println!("Rendering error: {}", e);
+            return None;
+        }
+    }
+
+    let video_maker = app
+        .ctx
+        .input_dir
+        .join(format!("{}/make_video.sh", task.mode));
+    let video_maker = path_util::to_slash(&video_maker).expect("Can't fix path to make_video.sh");
+    println!(
+        "Running bash: {} at {}",
+        video_maker.to_str().unwrap_or("unknown"),
+        app.ctx.output_dir.to_str().unwrap_or("unknown")
+    );
+    let mut command = if cfg!(windows) {
+        Command::new("C:/Program Files/Git/usr/bin/bash.exe")
+    } else {
+        Command::new("/bin/bash")
+    };
+    let output = command
+        .current_dir(app.ctx.output_dir.to_str().unwrap())
+        .arg("-c")
+        .arg(video_maker)
+        .output()
+        .expect("Failed to execute script");
+
+    // Print the output of the script
+    println!("Status: {}", output.status);
+    println!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    let file = app.ctx.output_dir.join("digest.mp4");
+    NamedFile::open(file).await.ok()
+}
+
 #[launch]
 fn rocket() -> _ {
     SimpleLogger::new()
@@ -172,7 +291,7 @@ fn rocket() -> _ {
         );
     }
 
-    rocket::build().mount("/", routes![index, digest, image])
+    rocket::build().mount("/", routes![index, digest, image, video])
 }
 
 // async fn async_main() -> Result<()> {
