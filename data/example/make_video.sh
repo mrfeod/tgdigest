ea524136336471676c4b5a7cbd81f4eb55a0c93e
Anton Sosnin <antsosnin@yandex.ru>
2026-04-29 15:13:28 +0300

    Refactor video creation script to use file list for ffmpeg processing

--- data/example/make_video.sh
+++ data/example/make_video.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-threshold_height=1750
 card_seconds=5
 
 shopt -s nullglob
@@ -11,31 +10,15 @@ if [ ${#cards[@]} -eq 0 ]; then
     exit 1
 fi
 
-inputs=()
+filelist=$(mktemp /tmp/ffmpeg_cards_XXXXXX.txt)
+trap 'rm -f "$filelist"' EXIT
 for card in "${cards[@]}"; do
-    inputs+=(-loop 1 -t "$card_seconds" -i "$card")
+    printf "file '%s/%s'\n" "$PWD" "$card" >> "$filelist"
+    printf "duration %s\n" "$card_seconds" >> "$filelist"
 done
 
-filters=()
-video_labels=()
-for i in "${!cards[@]}"; do
-    image_height=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=p=0 "${cards[$i]}")
-    if [ "${image_height:-0}" -gt "$threshold_height" ]; then
-        filters+=("[$i:v]crop=in_w:1680:0:0[top$i];[$i:v]crop=in_w:70:0:in_h-70[bottom$i];[top$i][bottom$i]vstack=inputs=2[crop$i]")
-        source="[crop$i]"
-    else
-        source="[$i:v]"
-    fi
-
-    filters+=("${source}pad=1080:1920:(ow-iw)/2:150:white,fps=30,format=yuv420p,setsar=1,setpts=PTS-STARTPTS[v$i]")
-    video_labels+=("[v$i]")
-done
-
-filters+=("${video_labels[*]}concat=n=${#cards[@]}:v=1:a=0[v]")
-filter_complex=$(IFS=';'; echo "${filters[*]}")
-
-ffmpeg -y "${inputs[@]}" \
-    -filter_complex "$filter_complex" \
-    -map "[v]" -an \
-    -r 30 -c:v libx264 -pix_fmt yuv420p \
+ffmpeg -y \
+    -f concat -safe 0 -i "$filelist" \
+    -vsync vfr \
+    -c:v libx264 -preset ultrafast -pix_fmt yuv420p \
     digest.mp4
